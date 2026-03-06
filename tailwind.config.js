3aa5745c01e530daf082095fa0da7096306e9239
17benabdallah-hue <17benabdallah@gmail.com>
2026-03-06 05:01:54 +0000

    Update tailwind.config.js
    Signed-off-by: 17benabdallah-hue <17benabdallah@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,6 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // لتفعيل الوضع الداكن باستخدام class
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {}, // هنا يمكنك إضافة تخصيصاتك مثل الألوان أو الخطوط
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'), // إذا كنت تستعمل إضافة typography
-  ],
-};
+}
